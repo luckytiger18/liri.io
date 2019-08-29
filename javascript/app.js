50769f787fea438e9b66f5e58d37180d9556b5bd
Annie Luc <lannie3186@gmail.com>
2019-08-28 21:24:38 -0700

    added function to concert-this

--- javascript/app.js
+++ javascript/app.js
@@ -1,29 +1,43 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
+var spotify = new Spotify(keys.spotify);
+
 var fs = require('fs');
 var request = require("request");
 
 var operation = process.argv[2];
 var input = process.argv[3];
 
-function getMusic() {
-    console.log("getting music")
+function getMusic(musicName) {
+    console.log("getting music");
+    request
 }
 
-function getMovie() {
+function getMovie(movieName) {
     console.log("getting movie");
-    request("http://www.omdbapi.com/?t="+input+"&apikey=8c35a259", function(error, response, body){
+    request("http://www.omdbapi.com/?t=" + movieName + "&apikey=8c35a259", function (error, response, body) {
         console.log(JSON.parse(body));
     });
 }
 
-function getConcert() {
-    console.log("getting concert")
+function getConcert(concertName) {
+    console.log("getting concert");
+    request("https://rest.bandsintown.com/artists/" + concertName + "/events?app_id=codingbootcamp", function (error, response, body) {
+        // console.log(JSON.parse(body))
+        var jsonBody = JSON.parse(body);
+        console.log("Venue: " + jsonBody[0].venue.name);
+        console.log("Location: " + jsonBody[0].venue.city + ", " + jsonBody[0].venue.region);
+        console.log("Date Time: " + jsonBody[0].datetime);
+    })
 }
-
-function doWhatItSays() {
+function doWhatItSays(sayWhat) {
     console.log("Understand?")
 }
-if(operation==="movie-this"){
-    getMovie();
+if (operation === "movie-this") {
+    getMovie(input);
+} else if (operation === "concert-this") {
+    getConcert(input);
 }
 
 
