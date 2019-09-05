9311085cb7f2379c84e10646ff4373c19fd575ff
Annie Luc <lannie3186@gmail.com>
2019-09-04 21:54:37 -0700

    added function doWhatItSays

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var Spotify = require("node-spotify-api")
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var http = require("http");
 
 var fs = require('fs');
 var request = require("request");
@@ -10,59 +11,83 @@ var operation = process.argv[2];
 var input = process.argv[3];
 
 function getMusic(musicName) {
-    console.log("getting music");
-    request
+
+    spotify.search({ type: 'track', query: musicName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Artist's Name: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Spotify Link : " + data.tracks.items[0].album.external_urls.spotify);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
+    })
 }
 
 function getMovie(movieName) {
-    console.log("getting movie");
+
     request("http://www.omdbapi.com/?t=" + movieName + "&apikey=8c35a259", function (error, response, body) {
-        console.log(JSON.parse(body));
+        var jsonBody = JSON.parse(body);
+        console.log("Title: " + jsonBody.Title);
+        console.log("Year: " + jsonBody.Year);
+        console.log("Country: " + jsonBody.Country);
+        console.log("Plot: " + jsonBody.Plot);
+        console.log("Actors: " + jsonBody.Actors);
+        console.log("IMDB Rating: " + jsonBody.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + jsonBody.Ratings[1].Value);
+        console.log("Metacritic Rating: " + jsonBody.Ratings[2].Value);
     });
 }
 
 function getConcert(concertName) {
-    console.log("getting concert");
+
     request("https://rest.bandsintown.com/artists/" + concertName + "/events?app_id=codingbootcamp", function (error, response, body) {
-        // console.log(JSON.parse(body))
         var jsonBody = JSON.parse(body);
-        console.log("Venue: " + jsonBody[0].venue.name);
-        console.log("Location: " + jsonBody[0].venue.city + ", " + jsonBody[0].venue.region);
-        console.log("Date Time: " + jsonBody[0].datetime);
+        for (let i = 0; i < jsonBody.length; i++) {
+            console.log("Venue: " + jsonBody[i].venue.name);
+            console.log("Location: " + jsonBody[i].venue.city + ", " + jsonBody[i].venue.region);
+            console.log("Date Time: " + jsonBody[i].datetime);
+            console.log("");
+        }
     })
 }
-function doWhatItSays(sayWhat) {
-    console.log("Understand?")
+function doWhatItSays() {
+
+    fs.readFile("./random.txt", "utf8", function (err, data) {
+        console.log("Understand?");
+        var strSplit = data.split(",");
+        var fileOperation = strSplit[0];
+        var fileInput = strSplit[1];
+        
+        if (fileOperation == "movie-this") {
+            if (fileInput == undefined) {
+                fileInput = "mr.nobody";
+            }
+            getMovie(fileInput);
+        } else if (fileOperation == "concert-this") {
+            getConcert(fileInput);
+        } else if (fileOperation == "spotify-this-song") {
+            if (fileInput == undefined) {
+                fileInput = 'The Sign Ace of Base';
+            }
+            getMusic(fileInput);
+        } 
+    })
 }
-if (operation === "movie-this") {
+
+if (operation == "movie-this") {
+    if (input == undefined) {
+        input = "mr.nobody";
+    }
     getMovie(input);
-} else if (operation === "concert-this") {
+
+} else if (operation == "concert-this") {
     getConcert(input);
+
+} else if (operation == "spotify-this-song") {
+    if (input == undefined) {
+        input = 'The Sign Ace of Base';
+    }
+    getMusic(input);
+} else if (operation == "do-what-it-says") {
+    doWhatItSays();
 }
-
-
-// if (operation == "spotify-this-song") {
-//     return getMusic();
-// } else if (operation == "movie-this") {
-//     return getMovie();
-// } else if (operation == "concert-this") {
-//     return getConcert();
-// } else if (operation == "do-what-it-says") {
-//     return doWhatItSays();
-// }else {
-//     console.log("unsupported operation")
-// }
-// // get parameter and put it in the function when after inputting the lines. example mode app.js movie-this spiderman
-// console.log(operation)
-
-// var r = require('request');
-
-// // var movie = process.argv[2];
-// var movie = process.argv.slice(2).join(' ');
-
-// var apiCall = 'https://www.omdbapi.com/?t=' + movie + '&y=&plot=short&apikey=trilogy';
-
-// r(apiCall, function (error, response, data) {
-// 	// console.log(data);
-// 	console.log(JSON.parse(data).Plot);
-// });
